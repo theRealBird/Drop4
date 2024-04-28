e6535566908c37f07840eb9d79a18fe000b272b8
theRealBird <adityab1621@gmail.com>
2024-04-28 10:35:55 +0530

    add instructions

--- funcs/conflict.cpp
+++ funcs/conflict.cpp
@@ -1,6 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+//INSTRUCTIONS TO REPLICATE
+//Take pull of main
+// change x = max(y,11) to max(y,10)
+//Pull upstream's test into your local main
+// Upstream's test has x = max(y,12,z)
+
 int testFunc (int a){
     int y = 9;
     int z = 0;
