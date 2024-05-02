317072750b515cd0f2cde8963ce5bb1732443a4d
Amogh Dixit <amoghdixit104@gmail.com>
2024-05-03 00:17:24 +0530

    test commit

--- funcs/conflict.cpp
+++ funcs/conflict.cpp
@@ -1,11 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-//INSTRUCTIONS TO REPLICATE
-//Take pull of main
-// change x = max(y,11) to max(y,10)
-//Pull upstream's test into your local main
-// Upstream's test has x = max(y,12,z)
 
 int testFunc (int a){
     int y = 9;
