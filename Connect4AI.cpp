1f2a167dbc440eb8435ddd1c828e89e0e125a8d3
Amogh Dixit <amoghdixit104@gmail.com>
2024-05-02 22:00:32 +0530

    test commit

--- Connect4AI.cpp
+++ Connect4AI.cpp
@@ -15,7 +15,7 @@ int AlphaBeta(const BitBoard &P, int alpha, int beta) {
           return (BitBoard::WIDTH*BitBoard::HEIGHT+1 - P.NumberofMoves())/2;
       }
       
-  } // check if current player can win nexxxxxt move
+  } // check if current player can win next move
     
 
   int maxx = (BitBoard::WIDTH*BitBoard::HEIGHT-1 - P.NumberofMoves())/2;	// upper bound of our score as we cannot win immediately
